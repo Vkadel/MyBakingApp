aa14ea9752c805c8ff0c840280ebe1061283404d
Vkadel <35178154+Vkadel@users.noreply.github.com>
2018-10-07 15:50:02 -0400

    Finished with test

---
+++ app/src/androidTest/java/com/example/virginia/mybakingapp/RecipeListActivityTestIngredientScreenExist.java
@@ -0,0 +1,82 @@
+package com.example.virginia.mybakingapp;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RecipeListActivityTestIngredientScreenExist {
+
+    @Rule
+    public ActivityTestRule<RecipeListActivity> mActivityTestRule = new ActivityTestRule<>(RecipeListActivity.class);
+
+    @Test
+    public void recipeListActivityTestIngredientScreenExist() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.recipe_list),
+                        childAtPosition(
+                                withId(R.id.frameLayout),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.recipe_detail_ingredients), withText("Ingredient List\n\n2.0 CUP Graham Cracker crumbs\n6.0 TBLSP unsalted butter, melted\n0.5 CUP granulated sugar\n1.5 TSP salt\n5.0 TBLSP vanilla\n1.0 K Nutella or other chocolate-hazelnut spread\n500.0 G Mascapone Cheese(room temperature)\n1.0 CUP heavy cream(cold)\n4.0 OZ cream cheese(softened)\n"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.recipe_detail_container),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
